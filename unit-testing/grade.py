c30d4aaf629bf17281876769fa8740045ef3ed6d
Matthew Terwilliger <matt@terwilligers.com>
2017-04-01 19:00:22 -0400

    Initial commit of autograder and rpn grader

---
+++ unit-testing/grade.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+import csv
+import json
+import sys
+
+sys.path.append('..')
+
+import autograder
+from test_cases import *
+
+if len(sys.argv) != 2:
+    print('Usage: ./grade.py submissions.csv')
+    exit()
+
+uniq_to_repo = {}
+with open(sys.argv[1]) as csvf:
+    for row in csv.DictReader(csvf):
+        uniq = row['Email Address'].split('@')[0]
+        repo = row['Give the link to your github repository']
+        uniq_to_repo[uniq] = repo
+
+test_cases = [
+    TestTravis(),
+    TestExponentiationGood(),
+    TestExponentiationBad(),
+    TestExponentiationImpl(),
+]
+
+def gen_submissions():
+    for uniq, repo in uniq_to_repo.items():
+        yield uniq, (repo, '/tmp/c4cs-rpn/{}'.format(uniq))
+
+submissions = {
+    uniq: submission for uniq, submission in gen_submissions()
+}
+
+ag = autograder.Autograder(test_cases, submissions)
+
+ag.clone()
+
+ag.grade()
+
+with open('results.json', 'w') as f:
+    f.write(ag.to_json())
+
+ag.print_stats()
---
+++ unit-testing/test_cases.py
@@ -0,0 +1,143 @@
+import autograder
+
+import os
+import sh
+import yaml
+
+class TestTravis(autograder.TestCase):
+    points_possible = 1.0
+
+    valid_test_scripts = [
+        'test_rpn.py',
+        'python -m unittest',
+        'python3 -m unittest',
+        'python test_rpn.py',
+        'coverage run test_rpn.py',
+        'coverage run -m unittest',
+        'coverage run -m  unittest',
+        'coverage run python3 -m unittest',
+        'make',
+        'make test',
+        'nosetests',
+        'nosetests --with-coverage',
+        'nosetests --with-coverage --cover-erase --cover-package=rpn --cover-tests',
+    ]
+
+    def test(self, repo_path):
+        with sh.pushd(repo_path):
+            if not os.path.exists('.travis.yml'):
+                return self.result('No .travis.yml file', 0)
+            try:
+                travis = yaml.load(open('.travis.yml'))
+
+                for script in self.valid_test_scripts:
+                    if script in travis['script']:
+                        return self.result('Travis set up to run tests', 1)
+
+                print(travis['script'])
+                allow = input("Give credit? [y/N] ").strip().upper()
+                if len(allow) and allow[0] == 'Y':
+                    return self.result('Travis set up to run tests', 1)
+                else:
+                    return self.result('Travis does not appear to run tests', 0)
+            except Exception as exc:
+                return self.result('Failed to parse .travis.yml as valid YAML',
+                                   0, additional_text=str(exc))
+
+# Test exponentiation ourselves, then monkey patch to break calculate
+# function and verify that exponentiation test works
+# golden lines should be
+# ['>>> 8', '>>> None', '>>> -1', '>>> GoodFail']
+ugly_one_liner = '''
+import inspect
+import traceback
+import rpn
+import test_rpn
+c = rpn.calculate
+T = test_rpn.TestBasics()
+fns = inspect.getmembers(T, predicate=lambda x: inspect.ismethod(x) and 'test_' in x.__name__ and 'test_add' not in x.__name__ and 'test_subtract' not in x.__name__ and 'test_multiply' not in x.__name__ and 'test_divide' not in x.__name__ and 'test_badstring' not in x.__name__)
+if len(fns) > 1:
+    for f in fns:
+        if f[0] == 'test_carat' or f[0] == 'test_exp' or f[0] == 'test_exponent' or f[0] == 'test_power3' or f[0] == 'test_exponentiation' or f[0] == 'test_power' or f[0] == 'test_expo' or f[0] == 'test_exponentiate' or f[0] == 'test_exponant':
+            test_fn = f[1]
+            break
+    else:
+        print(fns)
+        assert(len(fns) == 1)
+elif len(fns) == 0:
+    test_fn = None
+else:
+    test_fn = fns[0][1]
+try:
+    print(">>> {}".format(rpn.calculate("2 3 ^")))
+except Exception as e:
+    print(">>> Fail our ^ test. " + traceback.format_exc().replace("\\n", "ç"))
+try:
+    if test_fn:
+        print(">>> {}".format(test_fn()))
+    else:
+        print(">>> !! NO TEST FN")
+except Exception as e:
+    print(">>> Fail to pass student ^ test function. " + traceback.format_exc().replace("\\n", "ç"))
+test_rpn.rpn.calculate = lambda x: -1 if "^" in x else c(x)
+try:
+    print(">>> {}".format(rpn.calculate("2 3 ^")))
+except Exception as e:
+    print(">>> Fail ^ after monkey patch. " + traceback.format_exc().replace("\\n", "ç"))
+try:
+    if test_fn:
+        print(">>> {}".format(test_fn()))
+    else:
+        print(">>> !! NO TEST FN")
+except T.failureException:
+    print(">>> GoodFail")
+except Exception as e:
+    print(">>> Fail ^ (bad) test function threw an unknown exception. " + traceback.format_exc().replace("\\n", "ç"))
+'''
+
+class TestExponentiation(autograder.TestCase):
+    def get_lines(self, repo_path):
+        with sh.pushd(repo_path):
+            a = sh.python3('-c', ugly_one_liner)
+            lines = [x for x in a.stdout.decode('utf-8').split('\n') if '>>>' in x]
+            return lines
+
+class TestExponentiationGood(TestExponentiation):
+    points_possible = 1
+
+    def test(self, repo_path):
+        lines = self.get_lines(repo_path)
+
+        if lines[1] == '>>> None':
+            return self.result('Test case passes valid exponentiation function', 1)
+        elif lines[1] == '>>> !! NO TEST FN':
+            return self.result('Could not find test case for exponentiation', 0)
+        else:
+            return self.result('Testing for valid exponentiation function failed',
+                               0, additional_text=lines[1].replace('ç', '\n'))
+
+class TestExponentiationBad(TestExponentiation):
+    points_possible = 1
+
+    def test(self, repo_path):
+        lines = self.get_lines(repo_path)
+
+        if lines[3] == '>>> GoodFail':
+            return self.result('Test case catches bad exponentiation implmentation', 1)
+        elif lines[3] == '>>> !! NO TEST FN':
+            return self.result('Could not find test case for exponentiation', 0)
+        else:
+            return self.result('Testing that bad exponentiation function fails failed',
+                0, additional_text=lines[3].replace('ç', '\n'))
+
+class TestExponentiationImpl(TestExponentiation):
+    points_possible = 1
+
+    def test(self, repo_path):
+        lines = self.get_lines(repo_path)
+
+        if lines[0] == '>>> 8' or lines[0] == '>>> 8.0':
+            return self.result('Exponentiation implementation correct', 1)
+        else:
+            return self.result('Testing student exponentiation function failed',
+                               0, additional_text=lines[0].replace('ç', '\n'))
